8dc59e3538e6404749de75a5bf55628bb5a75abc
Martin Diers <martin.diers@thebernardgroup.com>
2022-02-22 12:20:26 -0600

    Add search and filtering to Software admin

--- asaudio/software/admin.py
+++ asaudio/software/admin.py
@@ -3,15 +3,17 @@ from .models import Developer, Category, Software
 
 
 class DeveloperAdmin(admin.ModelAdmin):
-    list_display = ["name", "url"]
+    list_display = ("name", "url",)
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ["name"]
+    list_display = ("name",)
 
 
 class SoftwareAdmin(admin.ModelAdmin):
-    list_display = ["name", "developer", "category"]
+    list_display = ("name", "version", "developer", "category",)
+    list_filter = ("category",)
+    search_fields = ("name", "developer__name")
 
 
 admin.site.register(Developer, DeveloperAdmin)
